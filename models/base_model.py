ee97859c77b045384e831719987078521b16f873
Idongesit Ekanem <aidisapp94@gmail.com>
2024-03-09 21:00:09 +0100

    Rechecked for updates and error issues

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,52 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
+
 """Module for Base class
 This actaully contains the Base class for the AirBnB clone console
 """
 
 import uuid
 from datetime import datetime
+
+
+class BaseModel:
+    """
+    This class represents the base model for all other classes.
+    It contains common attributes and methods.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S.%f')
+                                )
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the 'updated_at' attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of the BaseModel instance.
+
+        Returns:
+            dict: Dictionary containing all instance attributes.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import sys
-sys.path.append('..')
 
 from models.base_model import BaseModel
 
