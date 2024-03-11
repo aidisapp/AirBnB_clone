23a33ce9b3753ea122581ddd61c7ce167667a19d
Idongesit Ekanem <aidisapp94@gmail.com>
2024-03-12 00:30:33 +0100

    console.py error fixes

--- console.py
+++ console.py
@@ -16,6 +16,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class HBNBCommand(cmd.Cmd):
     """HolbertonBnB Command Processor.
 
@@ -182,6 +183,7 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     obj.__dict__[k] = v
         storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
