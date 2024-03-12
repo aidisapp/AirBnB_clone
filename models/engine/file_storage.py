9dcbc84bdd7750397d7c28fdb723e906928edc26
Idongesit Ekanem <aidisapp94@gmail.com>
2024-03-12 02:14:14 +0100

    A few Pycodestyle errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,19 +6,25 @@ serializing instances to a JSON file and deserializing JSON files to instances.
 """
 
 import json
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+import os
+import datetime
+from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+
+from models.review import Review
 
 
 class FileStorage:
     """
     File storage class
     """
-    
+    __file_path = "file.json"
+    __objects = {}
+
     def __init__(self):
         """
         Initialize the FileStorage instance.
@@ -57,3 +63,31 @@ class FileStorage:
                 self.__objects = json.load(f)
         except FileNotFoundError:
             pass
+
+    def classes(self):
+        """
+        Returns a dictionary of valid classes and their references
+        """
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
+        return classes
+
+    def reload(self):
+        """
+        Deserialize the JSON file to __objects
+        """
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    class_name = key.split('.')[0]
+                    if class_name in self.classes():
+                        self.__objects[key] = self.classes()[class_name](
+                                                                    **value)
