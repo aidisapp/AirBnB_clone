d6c8eee41b124fd607936142a82247edfc622fb1
Idongesit Ekanem <aidisapp94@gmail.com>
2024-03-13 14:43:26 +0100

    Base Model Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,60 @@
 #!/usr/bin/python3
-
-"""
-Module for BaseModel class
+"""Module for BaseModel class
 This module contains the BaseModel class representing the base model
-for all other classes. It contains common attributes and methods.
-"""
+for all other classes. It contains common attributes and methods."""
 
 import uuid
 from datetime import datetime
-from models import storage  # Import storage here
+from models import storage
 
 
 class BaseModel:
-    """
-    BaseModel class
-    """
+
+    """This class represents the base model of the object hierarchy."""
 
     def __init__(self, *args, **kwargs):
+        """This method initializes a Base instance.
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-value arguments
         """
-        Initialize the BaseModel instance.
-        If it's a new instance (not from a dictionary representation),
-        add a call to the method new(self) on storage.
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)  # Now you can use storage here
-
-    def save(self):
-        """
-        Update the 'updated_at' attribute with the current datetime.
-        Call save(self) method of storage.
-        """
-        self.updated_at = datetime.now()
-        storage.save()  # Now you can use storage here
-
-    def to_dict(self):
-        """
-        Return a dictionary representation of the BaseModel instance.
-        """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+            storage.new(self)
 
     def __str__(self):
-        """
-        Return a string representation of the BaseModel instance.
-        """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """This method returns a human-readable string representation
+        of an instance."""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """This method updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """This method returns a dictionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
