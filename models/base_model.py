0199a39193b7d5dd0b428fa38efb31099d0d3957
Idongesit Ekanem <aidisapp94@gmail.com>
2024-03-19 08:58:46 +0100

    Updates on base_model task

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,6 @@ from models import storage
 
 
 class BaseModel:
-
     """This class represents the base model of the object hierarchy."""
 
     def __init__(self, *args, **kwargs):
@@ -37,22 +36,20 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """This method returns a human-readable string representation
-        of an instance."""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        """This method returns a human-readable
+        string representation of an instance."""
+        return "[{}] ({}) {}".format(type(
+                    self).__name__, self.id, self.__dict__
+                    )
 
     def save(self):
         """This method updates the updated_at attribute
         with the current datetime."""
-
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """This method returns a dictionary representation of an instance."""
-
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
